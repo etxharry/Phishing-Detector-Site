0d2acaf8932b24af171276db541b6694b43b6f39
Huraira <maherhurara75@gmail.com>
2023-01-15 01:34:31 +0500

    PDS

---
+++ app.py
@@ -0,0 +1,34 @@
+import requests
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+api_key = "YOUR_API"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/phishing', methods=['POST'])
+def detect_phishing():
+    url = request.json["url"]
+    response = requests.post(f"https://safebrowsing.googleapis.com/v4/threatMatches:find?key={api_key}", json={
+        "client": {
+            "clientId":      "Phishing dectector", # replace yourProjectName with the actual project name
+            "clientVersion": "1.0.0"  # replace with the version of your application
+        },
+        "threatInfo": {
+            "threatTypes":      ["MALWARE", "SOCIAL_ENGINEERING"],
+            "platformTypes":    ["ANY_PLATFORM"],
+            "threatEntryTypes": ["URL"],
+            "threatEntries": [
+                {"url": url}
+            ]
+        }
+    })
+    if 'matches' in response.json():
+        return jsonify(is_phishing=True)
+    else:
+        return jsonify(is_phishing=False)
+
+if __name__ == '__main__':
+    app.run(debug=True)
